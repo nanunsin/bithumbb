390f48a327a4487e35c46fd5ae661f392ab94e60
nanunsin <nanunsin@gmail.com>
2018-01-09 00:03:09 +0900

    buy coin

--- bithumb/FuncBuy.go
+++ bithumb/FuncBuy.go
@@ -81,6 +81,42 @@ func (b *Bithumb) BuyPlaceETH(price int, units float64) (info [5]Market_Info) {
 	return
 }
 
+func (b *Bithumb) BuyPlace(coin string, price int, units float64) (info [5]Market_Info) {
+
+	var market_json_info market_buy_json_rec
+	params := fmt.Sprintf("price=%d&units=%.2f&order_currency=%s&type=bid", price, units, coin)
+
+	fmt.Printf("params: %s\n", params)
+	resp_data_str := b.apiCall("/trade/place", params)
+
+	resp_data_bytes := []byte(resp_data_str)
+
+	err := json.Unmarshal(resp_data_bytes, &market_json_info)
+	if err != nil {
+		log.Printf("%s\n", resp_data_str)
+		log.Println(err.Error())
+		return
+	}
+
+	if market_json_info.Status == "0000" {
+		fmt.Printf(" - Order_id : %s\n", market_json_info.Order_id)
+		for i, value := range market_json_info.Data {
+			fmt.Printf("[%d]\n", i)
+			fmt.Printf(" - ContID : %s\n", value.ContID)
+			fmt.Printf(" - Price : %.2f\n", value.Price)
+			fmt.Printf(" - Total : %.2f\n", value.Total)
+
+			info[i].ContID = value.ContID
+			info[i].Price = value.Price
+			info[i].Units = value.Units
+		}
+
+	} else {
+		log.Printf("%s\n", resp_data_str)
+	}
+	return
+}
+
 func (b *Bithumb) BuyETHEx(info *PlaceInfo, units float64) {
 
 	bDone := make(chan bool)
